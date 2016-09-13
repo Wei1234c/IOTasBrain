4e5ba62c3b1ca22231e2f33a6431bedc3f01ed40
Wei Lin <Wei1234c@gmail.com>
2016-09-14 00:44:09 +0800

    add .pdf file for PyCon JP 2016

--- celery_projects/celeryconfig.py
+++ celery_projects/celeryconfig.py
@@ -7,10 +7,10 @@ CELERY_TIMEZONE = 'Asia/Taipei'
 CELERYD_POOL_RESTARTS = True
 
 #__________________________________BROKER_URL__________________________________
-BROKER_URL = 'redis://192.168.0.114:6379/0'
+BROKER_URL = 'redis://netbrain.noip.me:6379/0'
 
 #____________________________CELERY_RESULT_BACKEND_____________________________
-CELERY_RESULT_BACKEND = 'redis://192.168.0.114:6379/1'
+CELERY_RESULT_BACKEND = 'redis://netbrain.noip.me:6379/1'
 
 #________________________________CELERY_IMPORTS________________________________
 CELERY_IMPORTS = ('IoT.neuron',)
